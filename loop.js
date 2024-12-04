513f594e7bf812aa5197674950553b2b27a1e175
Godspower <amriccygodspea@gmail.com>
2024-12-04 16:35:38 +0100

    Feat: Added Conditional Statements

---
+++ loop.js
@@ -0,0 +1,171 @@
+// // ---------- from 0 to 100 ----------------
+
+// // Use for loop to iterate from 0 to 100 and print only even numbers
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// // Use while loop to iterate from 0 to 100 and print only even numbers
+
+// let j = 0;
+// while (j <= 100) {
+//     if (j % 2 === 0) {
+//         console.log(j);
+//     }
+//     j++;
+// }
+
+// // Use do-while loop to iterate from 0 to 100 and print only even numbers
+
+// let k = 0;
+// do {
+//     if (k % 2 === 0) {
+//         console.log(k);
+//     }
+//     k++;
+// } while (k <= 100);
+
+// // Use for loop to iterate from 0 to 100 and print only odd numbers 
+
+// for (let a = 0; a <= 100; a++) {
+//     if (a % 2 === 1){
+//         console.log(a);
+//     }
+// }
+
+// // Use while loop to iterate from 0 to 100 and print only odd numbers
+
+// let b = 0;
+// while (b <= 100) {
+//     if (b % 2 === 1){
+//         console.log(b);
+//     }
+//     b++;
+// }
+
+// // Use do-while loop to iterate from 0 to 100 and print only odd numbers
+
+// let c = 0;
+// do {
+//     if (c % 2 === 1){
+//         console.log(c);
+//     }
+//     c++;
+// } while (c <= 100);
+
+// // ---------- from 100 to 0 ----------------
+
+// // Use for loop to iterate from 100 to 0 and print only even numbers
+
+// for (let d = 100; d >= 0; d--) {
+//     if (d % 2 === 0){
+//         console.log(d);
+//     }
+// }
+
+// // Use while loop to iterate from 100 to 0 and print only even numbers
+
+// let e = 100;
+// while (e >= 0) {
+//     if (e % 2 === 0){
+//         console.log(e);
+//     }
+//     e--;
+// }
+
+// // Use do-while loop to iterate from 100 to 0 and print only even numbers
+
+// let f = 100;
+// do {
+//     if (f % 2 === 0){
+//         console.log(f);
+//     }
+//     f--;
+// } while (f >= 0);
+
+// // Use for loop to iterate from 100 to 0 and print only odd numbers
+
+// for (let g = 100; g >= 0; g--) {
+//     if (g % 2 === 1){
+//         console.log(g);
+//     }
+// }
+
+// // Use while loop to iterate from 100 to 0 and print only odd numbers
+
+// let h = 100;
+// while (h >= 0) {
+//     if (h % 2 === 1){
+//         console.log(h);
+//     }
+//     h--;
+// }
+
+// // Use do-while loop to iterate from 100 to 0 and print only odd numbers
+
+// let i = 100;
+// do {
+//     if (i % 2 === 1){
+//         console.log(i);
+//     }
+//     i--;
+// } while (i >= 0);
+
+// Use for loop to iterate from 100 to 0 and print only numbers that are divisible by 3 and 5
+
+for (let j = 100; j >= 0; j--) {
+    if (j % 3 === 0 && j % 5 === 0){
+        console.log(j);
+    }
+}
+
+// Use while loop to iterate from 100 to 0 and print only numbers that are divisible by 3 and 5
+
+let k = 100;
+while (k >= 0) {
+    if (k % 3 === 0 && k % 5 === 0){
+        console.log(k);
+    }
+    k--;
+}
+
+// Use do-while loop to iterate from 100 to 0 and print only numbers that are divisible by 3 and 5
+
+let l = 100;
+do {
+    if (l % 3 === 0 && l % 5 === 0){
+        console.log(l);
+    }
+    l--;
+} while (l >= 0);
+
+// Use for loop to iterate from 100 to 0 and print only numbers that are divisible by 3 or 5
+
+for (let m = 100; m >= 0; m--) {
+    if (m % 3 === 0 || m % 5 === 0){
+        console.log(m);
+    }
+}
+
+// Use while loop to iterate from 100 to 0 and print only numbers that are divisible by 3 or 5
+
+let n = 100;
+while (n >= 0) {
+    if (n % 3 === 0 || n % 5 === 0){
+        console.log(n);
+    }
+    n--;
+}
+
+// Use do-while loop to iterate from 100 to 0 and print only numbers that are divisible by 3 or 5
+
+let o = 100;
+do {
+    if (o % 3 === 0 || o % 5 === 0){
+        console.log(o);
+    }
+    o--;
+} while (o >= 0);
